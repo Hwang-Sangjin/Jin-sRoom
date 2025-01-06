5ac6acd29eeb565da835935425c8fb4bae894879
Sangjin Hwang <aosdb5640@naver.com>
2025-01-07 01:47:42 +0900

    add: projection

--- src/components/Room/RoomCamping/Projection.jsx
+++ src/components/Room/RoomCamping/Projection.jsx
@@ -1,27 +1,26 @@
 import * as THREE from "three";
 import React, { useRef } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
-import BaseTexture from "&/Room/campingStuff.jpg";
+import BaseTexture from "&/Room/Projection.jpg";
 
-export function CampingStuff(props) {
-  const { nodes, materials } = useGLTF("/campingStuff.glb");
+export function Projection(props) {
+  const { nodes, materials } = useGLTF("/Projection.glb");
   const bakedTexture = useTexture(BaseTexture);
 
   bakedTexture.flipY = false;
   bakedTexture.colorSpace = THREE.SRGBColorSpace;
   bakedTexture.minFilter = THREE.LinearFilter;
   bakedTexture.magFilter = THREE.NearestFilter;
-
   return (
     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
-        geometry={nodes.Cylinder078.geometry}
-        material={nodes.Cylinder078.material}
-        position={[3.254, 1.498, -2.369]}
-        rotation={[Math.PI, -1.04, Math.PI]}
-        scale={0.291}
+        geometry={nodes.Cube149.geometry}
+        material={nodes.Cube149.material}
+        position={[2.731, 1.369, -3.557]}
+        rotation={[0, -Math.PI / 2, 0]}
+        scale={0.658}
       >
         <meshBasicMaterial map={bakedTexture} />
       </mesh>
@@ -29,4 +28,4 @@ export function CampingStuff(props) {
   );
 }
 
-useGLTF.preload("/campingStuff.glb");
+useGLTF.preload("/Projection.glb");
--- src/components/Room/RoomCamping/RoomCamping.jsx
+++ src/components/Room/RoomCamping/RoomCamping.jsx
@@ -2,12 +2,14 @@ import { CampingEmissive } from "./CampingEmissive";
 import { CampingSet } from "./CampingSet";
 import { CampingStuff } from "./CampingStuff";
 import { Carpet } from "./Carpet";
+import { Projection } from "./Projection";
 
 const RoomCamping = () => {
   return (
     <group>
       <Carpet />
       <CampingSet />
+      <Projection />
       <CampingStuff />
       <CampingEmissive />
     </group>
